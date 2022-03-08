ed192a40e79c43ef74450d43a783aa32537f572f
sarahsimoes03 <91324813+sarahsimoes03@users.noreply.github.com>
2022-03-08 10:58:26 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,142 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloud, cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var score=0;
+
+var PLAY=1;
+var END=0;
+var gameState= PLAY;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+  
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided" , trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+ 
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  
+}
+
+function draw() {
+  background(180);
+  //Sistema de Pontuação
+  text("Pontuação:"+score,50,50);
+  score= score+ Math.round(frameCount/100);
+
+  //Estados do Jogo
+  if(gameState=== PLAY){
+    ground.velocityX = -4;
+
+
+  }
+  else if (gameState=== END){
+// quando tocar no obstaculo 
+    ground.velocityX = 0;
+
+
+  }
+
+
+  // salto do trex
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -13;
+  }
+  // gravidade do trex
+  trex.velocityY = trex.velocityY + 0.8
+  // redefinir o solo
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  // colisão do trex com o solo invisivel
+  trex.collide(invisibleGround);
+  
+  //gerar as nuvens
+  spawnClouds();
+  
+  //gerar obstáculos no chão
+  spawnObstacles();
+  
+  drawSprites();
+}
+
+function spawnObstacles(){
+ if (frameCount % 60 === 0){
+   var obstacle = createSprite(400,165,10,40);
+   obstacle.velocityX = -6;
+
+   
+    // //gerar obstáculos aleatórios
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+   
+    //atribua dimensão e tempo de vida aos obstáculos         
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+ }
+}
+
+
+
+
+function spawnClouds() {
+  //escreva o código aqui para gerar as nuvens
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.y = Math.round(random(10,60));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //atribua tempo de vida à variável
+    cloud.lifetime = 200;
+    
+    //ajustar a profundidade
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+  }
+  
+}
